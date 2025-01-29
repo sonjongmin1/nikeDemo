a5d108a9c581e110cd6f6033e3e566cfde37cbf9
jong_min <whdals0525@gmail.com>
2025-01-29 14:38:51 +0900

    study

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ import Article from "./component/article/article.jsx";
 import Detail from "./pages/detail.jsx";
 import Error from "./pages/page404.jsx";
 import data from "./data/data.js";
-import data2 from "./data/data2.js";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/about.jsx";
-import axios from "axios";
 
 function App() {
   let [shoes] = useState(data);
-  let [shoes2, setShoes2] = useState(data2);
 
   return (
     <>
@@ -26,23 +23,7 @@ function App() {
               <MiddleBar />
               <Article shoes={shoes} />
               <div>
-                <button
-                  onClick={() => {
-                    axios
-                      .get("https://codingapple1.github.io/shop/data2.json")
-                      .then((result) => {
-                        // console.log(result.data);
-                        let copy = [...shoes2, ...result.data];
-                        setShoes2(copy);
-                        console.log(copy);
-                      })
-                      .catch(() => {
-                        console.log("서버 요청 실패");
-                      });
-                  }}
-                >
-                  데이터 가져오기
-                </button>
+                <button onClick={() => {}}>데이터 가져오기</button>
               </div>
             </>
           }
