e993bbc4985012dc0487373fb884d269232b9f0c
jong_min <whdals0525@gmail.com>
2025-01-30 22:00:22 +0900

    workflow

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
